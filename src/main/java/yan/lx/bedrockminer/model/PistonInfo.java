d5ec288c3357ad6573e21c58d851061743c316c3
Bunny_i <1160706050@qq.com>
2023-01-01 16:20:34 +0800

    Delete development residue files

--- src/main/java/yan/lx/bedrockminer/model/PistonInfo.java
+++
@@ -1,9 +0,0 @@
-package yan.lx.bedrockminer.model;
-
-import net.minecraft.block.Block;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-
-public record PistonInfo(Block block, BlockPos pos, Direction direction) {
-
-}
